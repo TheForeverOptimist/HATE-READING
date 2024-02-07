c6495865104cc85483ffa47740c399fa50b416a6
TheForeverOptimist <sean@elevate.app>
2024-02-07 14:47:16 -0800

    updated tailwind config again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,6 +75,6 @@ const config: Config = {
       }
     }
   },
-  plugins: [require("tailwindcss-animate"), require("tailwindcss/typography")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 export default config;
