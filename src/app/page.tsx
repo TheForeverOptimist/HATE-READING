988475800e2bf7044b3163f98bcc90bcad698c64
TheForeverOptimist <sean@elevate.app>
2024-02-09 23:02:17 -0800

    creating skeleton of feature section for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 hover:cursor-pointer">
           <p className="text-sm font-semibold text-gray-700">
-            DocuRead is public!
+            DocuTalk is public!
           </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
@@ -18,7 +18,7 @@ export default function Home() {
           seconds.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-800 sm:text-lg">
-          DocuRead allows you to have conversations with any PDF document.
+          DocuTalk allows you to have conversations with any PDF document.
           Simply upload your file and start asking questions right away.
         </p>
         <Link
@@ -77,6 +77,28 @@ export default function Home() {
             />
           </div>
         </div>
+      </div>
+
+      {/* Feature Section */}
+      <div className="mx-auto mb-32 max-w-5xl sm:mt-56">
+          <div className="mb-12 px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl sm:text-center">
+                <h2 className="mt-2 font-bold text-3xl text-gray-900 sm:text-5xl">
+                  Start chatting in minutes
+                </h2>
+                <p className="mt-4 text-lg text-gray-600">
+                  Chatting to your PDF files has never been easier than with DocuTalk.
+                </p>
+              </div>
+          </div>
+          {/* Steps */}
+          <ol className="my-8 space-y-8 pt-8 md:flex md:space-x-12 md:space-y-0">
+            <li className="md:flex-1">
+              <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                <span>Step 1</span>
+              </div>
+            </li>
+          </ol>
       </div>
     </>
   );
