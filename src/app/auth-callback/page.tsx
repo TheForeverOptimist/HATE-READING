517138b7e6107b5414ca4ade173c7b766cc68138
TheForeverOptimist <sean@elevate.app>
2024-02-15 14:39:14 -0800

    tRPC setup complete, prisma setup complete

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,10 +1,22 @@
 import { useRouter, useSearchParams } from "next/navigation"
+import { trpc } from "../_trpc/client";
+import { useEffect } from "react";
 
-const Page = () => {
+const Page = async () => {
     const router = useRouter()
 
     const searchParams = useSearchParams();
     const origin = searchParams.get('origin');
+
+    const {data} = trpc.authCallback.useQuery();
+
+    useEffect(() => {
+        if(data?.success) {
+            router.push(origin ? `/${origin}` : '/dashboard');
+        }
+    }, [data, router, origin]);
+
+
 }
 
 
