6bbe5316a2dd0de7692a7339fa56512c2d9fd3e6
TheForeverOptimist <sean@elevate.app>
2024-02-18 11:41:48 -0800

    updating trpc query requests to display files in dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,17 +1,26 @@
-import UploadButton from "./UploadButton"
-
+import { trpc } from "@/app/_trpc/client";
+import UploadButton from "./UploadButton";
 
 const Dashboard = () => {
-    return (
-        <main className="mx-auto max-w-7xl md:p-10">
-            <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
-                <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
-                <UploadButton />
-            </div>
+  const {data: files, isLoading} = trpc.getUserFiles.useQuery();
 
-        {/* display all user files */}
-        </main>
-    )
-}
+  return (
+    <main className="mx-auto max-w-7xl md:p-10">
+      <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
+        <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
+        <UploadButton />
+      </div>
 
-export default Dashboard
+      {/* display all user files */}
+      {files && files?.length !== 0 ? (
+        <div></div>
+      ) : isLoading ? (
+        <div></div>
+      ) : (
+        <div></div>
+      )}
+    </main>
+  );
+};
+
+export default Dashboard;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,5 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 
@@ -9,28 +9,36 @@ export const appRouter = router({
     const user = await getUser();
 
     if (!user || !user.id || !user.email) {
-      throw new TRPCError({ code: 'UNAUTHORIZED' });
+      throw new TRPCError({ code: "UNAUTHORIZED" });
     }
 
     // check if the user is in the database
     const dbUser = await db.user.findFirst({
       where: {
-        id: user.id
-      }
-    })
+        id: user.id,
+      },
+    });
 
-    if(!dbUser){
+    if (!dbUser) {
       //create user in db
       await db.user.create({
         data: {
           id: user.id,
-          email: user.email
-        }
-      })
+          email: user.email,
+        },
+      });
     }
 
-
     return { success: true };
+  }),
+  getUserFiles: privateProcedure.query(async({ ctx }) => {
+    const {userId, user} = ctx;
+
+    return await db.file.findMany({
+      where: {
+        userId
+      }
+    })
   }),
 });
 
