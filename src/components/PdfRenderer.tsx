c293465ba73d48bb4dc4dc66c8ca43acf5639467
TheForeverOptimist <sean@elevate.app>
2024-02-23 14:46:48 -0800

    added pdfjs worker to display pdf

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,16 +1,32 @@
-import { Page, Text, View, Document, StyleSheet} from '@react-pdf/renderer'
+"use client";
 
+import { Page, Document, pdfjs } from "react-pdf";
 
-const PdfRenderer = () => {
-    return (
-    <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
-        <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
-            <div className='flex items-center gap-1.5'>
-                top bar
-            </div>
-        </div>
-    </div>
-    )
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+interface PdfRendererProps {
+  url: string;
 }
 
-export default PdfRenderer
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">top bar</div>
+      </div>
+
+      <div className="flex-1 w-full max-h-screen">
+        <div>
+          <Document file={url} className="max-h-full">
+            <Page pageNumber={1} />
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PdfRenderer;
