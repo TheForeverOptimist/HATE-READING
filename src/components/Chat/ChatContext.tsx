b52deb63414cb99fb5275f9b0746ac0c33cc51d5
TheForeverOptimist <sean@elevate.app>
2024-03-02 12:18:58 -0800

    added optimistic updates in message/chat ux

--- src/components/Chat/ChatContext.tsx
+++ src/components/Chat/ChatContext.tsx
@@ -1,7 +1,8 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useRef, useState } from "react";
 import { useToast } from "../ui/use-toast";
 import { useMutation } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 
 type StreamResponse = {
   addMessage: () => void;
@@ -30,6 +31,8 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
 
   const { toast } = useToast();
 
+  const backupMessage = useRef('')
+
   const { mutate: sendMessage } = useMutation({
     mutationFn: async ({ message }: { message: string }) => {
       const response = await fetch("/api/message", {
@@ -45,9 +48,68 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
 
       return response.body;
     },
-    onMutate: () => {
-      
-    }
+    onMutate: async ({message}) => {
+      backupMessage.current = message
+      setMessage('')
+
+
+      //step 1
+      await utils.getFileMessages.cancel()
+
+      //step2
+      const previousMessages = utils.getFileMessages.getInfiniteData()
+
+      //step3
+      utils.getFileMessages.setInfiniteData(
+        {fileId, limit: INFINITE_QUERY_LIMIT},
+        (old) => {
+          if(!old) {
+            return {
+              pages: [],
+              pageParams: []
+            }
+          }
+
+          let newPages = [...old.pages]
+
+          let latestPage = newPages[0]!
+
+          latestPage.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true
+            },
+            ...latestPage.messages
+          ]
+
+          newPages[0] = latestPage
+
+          return {
+            ...old,
+            pages: newPages,
+          }
+        }
+      )
+        setIsLoading(true)
+
+        return {
+          previousMessages: previousMessages?.pages.flatMap((page) => page.messages) ?? [],
+        }
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current)
+      utils.getFileMessages.setData(
+        {fileId},
+        {messages: context?.previousMessages ?? []}
+      )
+    },
+    onSettled: async () => {
+      setIsLoading(false)
+
+      await utils.getFileMessages.invalidate({fileId})
+    },
   });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
--- src/components/Chat/Messages.tsx
+++ src/components/Chat/Messages.tsx
@@ -4,12 +4,17 @@ import { keepPreviousData } from "@tanstack/react-query";
 import { Loader2, MessageSquare } from "lucide-react";
 import Skeleton from "react-loading-skeleton";
 import Message from "./Message";
+import { useContext } from "react";
+import { ChatContext } from "./ChatContext";
 
 interface MessagesProps {
   fileId: string;
 }
 
 const Messages = ({ fileId }: MessagesProps) => {
+
+  const {isLoading: isAiThinking } = useContext(ChatContext)
+
   const { data, isLoading, fetchNextPage } =
     trpc.getFileMessages.useInfiniteQuery(
       {
@@ -34,7 +39,7 @@ const Messages = ({ fileId }: MessagesProps) => {
   };
 
   const combinedMessages = [
-    ...(true ? [loadingMessage] : []),
+    ...(isAiThinking ? [loadingMessage] : []),
     ...(messages ?? []),
   ];
 
