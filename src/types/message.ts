d4932f6335ed6e7ec25e21fe7b809b97fbbcd005
TheForeverOptimist <sean@elevate.app>
2024-03-01 16:29:16 -0800

    added icons for user and AI response, also completed message api logic

--- src/types/message.ts
+++ src/types/message.ts
@@ -5,3 +5,11 @@ import { inferRouterOutputs } from "@trpc/server";
 type RouterOutput = inferRouterOutputs<AppRouter>
 
 type Messages = RouterOutput['getFileMessages']['messages']
+
+type OmitText = Omit<Messages[number], "text">
+
+type ExtendedText = {
+    text: string | JSX.Element
+}
+
+export type ExtendedMessage = OmitText & ExtendedText
