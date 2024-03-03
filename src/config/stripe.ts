db6f409b22c54bd902af3252dd5515e00a4330a6
TheForeverOptimist <sean@elevate.app>
2024-03-02 16:06:13 -0800

    added stripe integration

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: "10",
+    pagesPerPDF: "5",
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: "50",
+    pagesPerPDF: "25",
+    price: {
+      amount: 9,
+      priceIds: {
+        test: "price_1Oq2VfGDZ3ypwTJvtYLtIrPM",
+        production: "",
+      },
+    },
+  },
+];
