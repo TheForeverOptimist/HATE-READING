8944bdba408980aa8c6514b0d63909d901a5b68b
TheForeverOptimist <sean@elevate.app>
2024-03-03 17:49:20 -0800

    added UserAccoutNav plus additional conditional logic for logged in user and not logged in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,31 @@
-import Link from "next/link"
-import MaxWidthWrapper from "./MaxWidthWrapper"
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import UserAccountNav from "./UserAccountNav";
 
-const Navbar = () => {
-    return (
-      <nav className="sticky h-20 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
-        <MaxWidthWrapper>
-          <div className="flex h-20 justify-between items-center border-b border-zinc-200">
-            <Link href="/" className="flex z-40 font-semibold">
-              <Image src="/doclogo.png" alt="logo" width={200} height={70} />
-            </Link>
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-            {/* todo: add mobile navbar */}
+  return (
+    <nav className="sticky h-20 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-20 justify-between items-center border-b border-zinc-200">
+          <Link href="/" className="flex z-40 font-semibold">
+            <Image src="/doclogo.png" alt="logo" width={200} height={70} />
+          </Link>
 
-            <div className="hidden items-center space-x-4 sm:flex">
+          {/* todo: add mobile navbar */}
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            {!user ? (
               <>
                 <Link
                   href="/pricing"
@@ -43,11 +52,32 @@ const Navbar = () => {
                   Get Started <ArrowRight className="ml-1.5 h-5 w-5" />
                 </RegisterLink>
               </>
-            </div>
+            ) : (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Dashboard
+                </Link>
+
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name ? "Your Account" : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ""}
+                  imageUrl={user.picture ?? ''}
+                />
+              </>
+            )}
           </div>
-        </MaxWidthWrapper>
-      </nav>
-    );
-}
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
 
 export default Navbar;
