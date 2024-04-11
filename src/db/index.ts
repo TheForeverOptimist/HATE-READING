aa84cb8b75f4c77a9854759033fcdd4b74cdd051
TheForeverOptimist <sean@elevate.app>
2024-04-11 09:39:53 -0700

    updated prisma schemas and integrated new neon db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+// import{Pool, neonConfig} from '@neondatabase/serverless'
+// import { PrismaNeon} from '@prisma/adapter-neon'
+// import ws from 'ws';
+// import dotenv from 'dotenv'
+
+// dotenv.config();
+
+// neonConfig.webSocketConstructor = ws;
+
+// const connectionString = process.env.DATABASE_URL;
+
+// const pool = new Pool({ connectionString});
+
+// const adapter = new PrismaNeon(pool)
 
 declare global {
   // eslint-disable-next-line no-var
